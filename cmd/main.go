b5170cc58a7673c6bef0474da6529d0e7636df48
Georgeo Rocco <ggrocco@gmail.com>
2022-12-26 18:58:23 -0300

    Add cmd main whit sample

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"in_memory_db/internal/db"
+	"in_memory_db/internal/interpreter"
+	"io"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Print("test")
+	db := db.New()
+	runLoop(db, os.Stdin, os.Stdout)
+}
+
+func runLoop(db db.DB, input io.Reader, output io.Writer) {
+	fmt.Println(" > type END to exit")
+
+	scanner := bufio.NewScanner(input)
+
+	var line string
+	for {
+		scanner.Scan()
+		line = scanner.Text()
+		if strings.EqualFold(line, "END") {
+			break
+		}
+
+		cmdOutput, err := interpreter.Run(db, line)
+		if err != nil {
+			output.Write([]byte(fmt.Sprintf("Fail on execute `%v`, error: %v\n", line, err)))
+			continue
+		}
+
+		if len(cmdOutput) > 0 {
+			cmdOutput += "\n"
+		}
+
+		output.Write([]byte(cmdOutput))
+	}
+
+	fmt.Println("see you soon")
 }
---
+++ cmd/main_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"in_memory_db/internal/db"
+	"strings"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	type interaction struct {
+		command string
+		output  string
+	}
+
+	testCases := []struct {
+		description  string
+		interactions []interaction
+	}{
+		{
+			description: "invalid",
+			interactions: []interaction{
+				{"SET with-one-arg", "Fail on execute `SET with-one-arg`, error: the command: SET, require 2 args"},
+				{"END", ""},
+			},
+		},
+		{
+			description: "basic",
+			interactions: []interaction{
+				{"SET test-var-name 100", ""},
+				{"GET test-var-name", "100"},
+				{"UNSET test-var-name", ""},
+				{"GET test-var-name", "Nil"},
+				{"SET test-var-name-1 50", ""},
+				{"SET test-var-name-2 50", ""},
+				{"NUMEQUALTO 50", "2"},
+				{"SET test-var-name-2 10", ""},
+				{"NUMEQUALTO 50", "1"},
+				{"END", ""},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			db := db.New()
+			var inputBuffer bytes.Buffer
+
+			for _, i := range tc.interactions {
+				inputBuffer.WriteString(i.command + "\n")
+			}
+
+			var outputBuffer bytes.Buffer
+			runLoop(db, &inputBuffer, &outputBuffer)
+
+			for _, i := range tc.interactions {
+				if i.output != "" {
+					output, err := outputBuffer.ReadString('\n')
+					if err != nil {
+						t.Errorf("read output fail, err: %v", err)
+					}
+
+					if i.output != strings.TrimSuffix(output, "\n") {
+						t.Errorf("Fail: expected %v to be %v", i.output, output)
+					}
+				}
+			}
+		})
+	}
+}
